f1c96874cfc6cd61e4a019bdf6568496311ee537
Doug Knight <dougbknight@gmail.com>
2024-02-03 16:19:26 -0600

    make create-db work

--- src/multi_money/db/sql/migrations.clj
+++ src/multi_money/db/sql/migrations.clj
@@ -21,10 +21,15 @@
     (repl/rollback cfg)
     (repl/migrate cfg)))
 
-(defn create-db []
-  (let [{:keys [dbname] :as cfg} (:datastore (config))
+(defn create-db
+  "Creates the database specified in the configuration.
+
+  We assume that by default there is a database with the same
+  name as the user. We connect using that database, and then create
+  the one that the app is configured to us."
+  []
+  (let [{:keys [dbname user] :as cfg} (db/config :sql)
         ds (-> cfg
-               (assoc :dbname "postgres")
-               (assoc :classname "org.postgresql.Driver")
+               (assoc :dbname user)
                j/get-datasource)]
-    (j/execute! ds ["create database %" dbname])))
+    (j/execute! ds [(format "create database %s" dbname)])))
