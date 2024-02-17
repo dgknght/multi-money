86f1575a69804f4cb9574af25174bcc51c2f7384
Doug Knight <dougbknight@gmail.com>
2024-02-17 16:25:59 -0600

    refine db strategy selection

--- src/multi_money/state.cljs
+++ src/multi_money/state.cljs
@@ -9,6 +9,7 @@
 
 (def current-page (r/cursor app-state [:current-page]))
 (def current-user (r/cursor app-state [:current-user]))
+(def db-strategy (r/cursor app-state [:db-strategy]))
 (def auth-token (r/cursor app-state [:auth-token]))
 (def nav-items (r/cursor app-state [:nav-items]))
 
@@ -17,4 +18,3 @@
 
 (defn -busy []
   (swap! app-state update-in [:process-count] dec))
-
--- src/multi_money/views/components.cljs
+++ src/multi_money/views/components.cljs
@@ -1,19 +1,18 @@
 (ns multi-money.views.components
   (:require [clojure.pprint :refer [pprint]]
             [camel-snake-kebab.core :refer [->kebab-case-string]]
+            [goog.string :refer [format]]
             [dgknght.app-lib.inflection :refer [humanize]]
-            [multi-money.icons :refer [icon]]
-            [multi-money.state :refer [nav-items]]))
+            [multi-money.icons :refer [icon
+                                       icon-with-text]]
+            [multi-money.state :refer [nav-items
+                                       db-strategy]]))
 
 (derive js/String ::string)
 (derive cljs.core/PersistentArrayMap ::map)
 (derive cljs.core/Keyword ::keyword)
 
 (defmulti ^:private expand-nav-item type)
-
-(defmethod expand-nav-item :default
-  [x]
-  (pprint {::no-expend-nav-item-for (type x)}))
 
 (defmethod expand-nav-item ::keyword [k] k)
 
@@ -38,12 +37,13 @@
     :divider [:li [:hr.dropdown-divider]]))
 
 (defmethod dropdown-item ::map
-  [{:keys [caption path on-click id]}]
+  [{:keys [caption path on-click id active?]}]
   ^{:key (str "dropdown-item-" id)}
   [:li
    [:a.dropdown-item
-    {:href path
-     :on-click on-click}
+    (cond-> {:href path
+             :on-click on-click}
+      active? (assoc :class "active"))
     caption]])
 
 (defn- dropdown
@@ -55,16 +55,19 @@
         doall)])
 
 (defn- nav-item
-  [{:keys [path on-click caption children id]}]
+  [{:keys [path on-click caption children id active?]}]
   ^{:key (str "nav-item-" id)}
   [:li.nav-item {:class (when (seq children) "dropdown")}
-   [:a.nav-link (merge {:href path
-                        :on-click on-click}
-                       (when (seq children)
-                         {:class "dropdown-toggle"
-                          :role :button
-                          :data-bs-toggle :dropdown
-                          :aria-expanded false}))
+   [:a.nav-link.d-flex.align-items-center
+    (merge {:href path
+            :on-click on-click}
+           (when active?
+             {:class "active"})
+           (when (seq children)
+             {:class "dropdown-toggle"
+              :role :button
+              :data-bs-toggle :dropdown
+              :aria-expanded false}))
     caption]
    (when (seq children)
      (dropdown children))])
@@ -76,18 +79,27 @@
                    expand-nav-item))
         doall)])
 
-(def ^:private unauthenticated-nav-items
-  [{:caption "DB Strategy"
-    :children [{:id "sql-db-strategy"
-                :caption "SQL"
-                :on-click #(println "change to sql")}
-               {:id "mongo-db-strategy"
-                :caption "MongoDB"
-                :on-click #(println "change to mongo")}]}])
+(defn- db-strategy-nav-item []
+  (let [current @db-strategy]
+    {:caption (icon-with-text :database current)
+     :active? true
+     :children (mapv (fn [s]
+                       {:id (format "%s-db-strategy" (name s))
+                        :active? (= s current)
+                        :caption s
+                        :on-click #(reset! db-strategy s)})
+                     [:sql :mongo])}))
+
+(defn- build-nav-items []
+  [(db-strategy-nav-item)])
 
 (defn title-bar []
+  (add-watch db-strategy
+             ::title-bar
+             (fn [& _]
+               (reset! nav-items (build-nav-items))))
   (when-not @nav-items
-    (reset! nav-items unauthenticated-nav-items))
+    (reset! nav-items (build-nav-items)))
   (fn []
     [:nav.navbar.navbar-expand-lg.bg-body-tertiary.rounded.mt-1
      {:aria-label "Primary Navigation Menu"}
