6bc7c1ab4ae3071db5b44a58b803975ffa223321
Doug Knight <dougbknight@gmail.com>
2024-04-14 11:18:06 -0500

    just add the env in the compose config

--- scripts/datomic-init.sh
+++ scripts/datomic-init.sh
@@ -1,13 +1,13 @@
-if [ "$( PGPASSWORD=$SQL_ADM_PASSWORD psql --host=$SQL_HOST --user=$SQL_ADM_USER -XtAc "SELECT 1 FROM pg_database WHERE datname='datomic'" )" = '1' ]
+if [ "$( psql --host=$SQL_HOST --user=$SQL_ADM_USER -XtAc "SELECT 1 FROM pg_database WHERE datname='datomic'" )" = '1' ]
 then
 	echo "datomic database already exists"
 else
 	echo "creating the datomic database..."
-	PGPASSWORD=$SQL_ADM_PASSWORD psql \
+	psql \
 		--file=./scripts/postgres-db.sql \
 		--username=$SQL_ADM_USER \
 		--host=$SQL_HOST
-	PGPASSWORD=$SQL_ADM_PASSWORD psql \
+	psql \
 		--file=./scripts/postgres-table.sql \
 		--username=$SQL_ADM_USER \
 		--host=$SQL_HOST \
