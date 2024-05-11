c13cb303574b8d54cac75ab2409bf0bcab0df836
Doug Knight <dougbknight@gmail.com>
2024-05-11 15:05:03 -0500

    use correct syntax for variadoc options

--- src/multi_money/api/entities.cljs
+++ src/multi_money/api/entities.cljs
@@ -3,23 +3,23 @@
   (:require [dgknght.app-lib.api-3 :as api]))
 
 (defn select
-  [& opts]
+  [& {:as opts}]
   (api/get (api/path :entities) opts))
 
-(defn create
+(defn- create
   [entity opts]
   (api/post (api/path :entities) entity opts))
 
-(defn update
+(defn- update
   [{:keys [id] :as entity} opts]
   (api/patch (api/path :entities id) entity opts))
 
 (defn put
-  [entity & opts]
+  [entity & {:as opts}]
   (if (:id entity)
     (update entity opts)
     (create entity opts)))
 
 (defn delete
-  [{:keys [id]} & opts]
+  [{:keys [id]} & {:as opts}]
   (api/delete (api/path :entities id) opts))
