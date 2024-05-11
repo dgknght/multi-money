aac2fcb43f0b9d5cc28d45aaf612202b2bdf1377
Doug Knight <dougbknight@gmail.com>
2024-05-11 15:53:16 -0500

    use local api ns

--- src/multi_money/api/entities.cljs
+++ src/multi_money/api/entities.cljs
@@ -1,18 +1,19 @@
 (ns multi-money.api.entities
   (:refer-clojure :exclude [update])
-  (:require [dgknght.app-lib.api-3 :as api]))
+  (:require [dgknght.app-lib.api :refer [path]]
+            [multi-money.api :as api]))
 
 (defn select
   [& {:as opts}]
-  (api/get (api/path :entities) opts))
+  (api/get (path :entities) opts))
 
 (defn- create
   [entity opts]
-  (api/post (api/path :entities) entity opts))
+  (api/post (path :entities) entity opts))
 
 (defn- update
   [{:keys [id] :as entity} opts]
-  (api/patch (api/path :entities id) entity opts))
+  (api/patch (path :entities id) entity opts))
 
 (defn put
   [entity & {:as opts}]
@@ -22,4 +23,4 @@
 
 (defn delete
   [{:keys [id]} & {:as opts}]
-  (api/delete (api/path :entities id) opts))
+  (api/delete (path :entities id) opts))
