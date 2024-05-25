2caacb99bf7795417cc05bbea26775fe87f6cd90
Doug Knight <dougbknight@gmail.com>
2024-05-25 12:00:26 -0500

    change api/entities test to use the db

--- test/multi_money/api/entities_test.clj
+++ test/multi_money/api/entities_test.clj
@@ -15,15 +15,15 @@
 (deftest an-authenticated-user-can-create-an-entity
   (with-context
     (let [res (request :post (path :api :entities)
-                       :json-body {:entity/name "Business"}
+                       :json-body {:entity/name "Side Hustle"}
                        :user (find-user "john@doe.com"))]
       (is (http-created? res))
       (is (:id (:json-body res))
           "The response includes an id")
-      (is (comparable? {:entity/name "Business"}
+      (is (comparable? {:entity/name "Side Hustle"}
                        (:json-body res))
           "The created entity is returned")
-      (is (comparable? {:entity/name "Business"}
+      (is (comparable? {:entity/name "Side Hustle"}
                        (ents/find (:json-body res))) 
           "The entity can be retrieved"))))
 
@@ -38,12 +38,11 @@
           "The response body contains the validation errors"))))
 
 (deftest an-unauthenticated-user-cannot-create-an-entity
-  (with-mocks [calls]
-    (let [count-before (ents/count)]
-      (is (http-unauthorized? (request :post (path :api :entities)
+  (let [count-before (ents/count)]
+    (is (http-unauthorized? (request :post (path :api :entities)
                                      :json-body {:entity/name "My Money"})))
-      (is (= count-before (ents/count))
-          "No entity is created"))))
+    (is (= count-before (ents/count))
+        "No entity is created")))
 
 (deftest an-authenticated-user-can-get-a-list-of-his-entities
   (with-context
@@ -59,71 +58,67 @@
           "The entities are returned"))))
 
 (deftest an-authenticated-user-can-update-his-entity
-  (testing "update an existing entity"
-    (with-context
+  (with-context
+    (testing "update an existing entity"
       (let [entity (find-entity "Personal")
-            res (request :patch (path :api :entities "201")
+            res (request :patch (path :api :entities (:id entity))
                          :user (find-user "john@doe.com")
-                         :json-body {:name "The new name"})]
+                         :json-body {:entity/name "The new name"})]
         (is (http-success? res))
-        (is (comparable? [{:entity/name "The new name"}]
-                         (ents/find entity))
-            "The updated entity can be retrieved")
-        (is (comparable? {:id "201"
-                          :entity/name "The new name"}
+        (is (comparable? {:entity/name "The new name"}
                          (:json-body res))
-            "The updated entity is returned"))))
-  (testing "attempt to update an non-existing entity"
-    (with-context
+            "The updated entity is returned")
+        (is (comparable? {:entity/name "The new name"}
+                         (ents/find entity))
+            "The updated entity can be retrieved")))
+    (testing "attempt to update an non-existing entity"
       (is (http-not-found? (request :patch (path :api :entities "999")
                                     :user (find-user "john@doe.com")
                                     :json-body {:name "The new name"}))))))
 
 (deftest an-authenticate-user-cannot-update-anothers-entity
-  (with-mocks [calls]
-    (is (http-not-found? (request :patch (path :api :entities "201")
-                                  :user {:id "102"}
-                                  :json-body {:name "The new name"})))
-    (is (empty? (:put @calls))
-        "The entities put fn is called once")))
+  (with-context
+    (let [entity (find-entity "Personal")]
+      (is (http-not-found? (request :patch (path :api :entities (:id entity))
+                                    :user (find-user "jane@doe.com")
+                                    :json-body {:name "The new name"})))
+      (is (not= "The new name"
+                (:name (ents/find entity)))
+          "The name is not updated in the database"))))
 
 (deftest an-unauthenticated-user-cannot-update-an-entity
-  (with-mocks [calls]
-    (is (http-unauthorized? (request :patch (path :api :entities "201")
-                                  :json-body {:name "The new name"})))
-    (is (empty? (:put @calls))
-        "The entities put fn is called once")))
+  (with-context
+    (let [entity (find-entity "Personal")]
+      (is (http-unauthorized? (request :patch (path :api :entities (:id entity))
+                                       :json-body {:name "The new name"})))
+      (is (not= "The new name"
+                (:name (ents/find entity)))
+          "The name is not updated in the database"))))
 
 (deftest delete-an-entity
-  (testing "delete an existing entity"
-    (with-mocks [calls]
-      (let [res (request :delete (path :api :entities "201")
-                         :user {:id "101"})
-            {[c :as cs] :delete} @calls]
+  (with-context
+    (testing "delete an existing entity"
+      (let [entity (find-entity "Personal")
+            res (request :delete (path :api :entities (:id entity))
+                         :user (find-user "john@doe.com"))]
         (is (http-no-content? res))
-        (is (= 1 (count cs))
-            "The delete function is called once")
-        (is (= [{:id "201"
-                 :entity/owner {:id "101"}
-                 :entity/name "Personal"}]
-               c)
-            "The delete function is called with the entity to be deleted"))))
-  (testing "attempt to delete a non-existing entity"
-    (with-mocks [calls]
+        (is (nil? (ents/find entity))
+            "The entity cannot be retrieved after delete")))
+    (testing "attempt to delete a non-existing entity"
       (is (http-not-found? (request :delete (path :api :entities "999")
-                                    :user {:id "101"})))
-      (is (empty? (:delete @calls))
-          "The delete fn is not called"))))
+                                    :user (find-user "john@doe.com")))))))
 
 (deftest an-authenticated-user-cannot-delete-anothers-entity
-  (with-mocks [calls]
-    (is (http-not-found? (request :delete (path :api :entities "201")
-                                  :user {:id "102"})))
-    (is (empty? (:delete @calls))
-        "The delete fn is not called")))
+  (with-context
+    (let [entity (find-entity "Personal")]
+      (is (http-not-found? (request :delete (path :api :entities (:id entity))
+                                    :user (find-user "jane@doe.com"))))
+      (is (ents/find entity)
+          "The entity can be retrieved after denied delete request"))))
 
 (deftest an-unauthenticated-user-cannot-delete-an-entity
-  (with-mocks [calls]
-    (is (http-unauthorized? (request :delete (path :api :entities "201"))))
-    (is (empty? (:delete @calls))
-        "The delete fn is not called")))
+  (with-context
+    (let [entity (find-entity "Personal")]
+      (is (http-unauthorized? (request :delete (path :api :entities (:id entity)))))
+      (is (ents/find entity)
+          "The entity can be retrieved after denied delete request"))))
