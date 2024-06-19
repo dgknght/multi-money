28c06bbacafd158128890a50643dca5ef1d1aebb
Doug Knight <dougbknight@gmail.com>
2024-06-19 11:58:34 -0500

    remove code moved to stowaway

--- src/multi_money/datalog.clj
+++ src/multi_money/datalog.clj
@@ -1,9 +1,7 @@
 (ns multi-money.datalog
   (:require [clojure.spec.alpha :as s]
-            [clojure.pprint :refer [pprint]]))
-
-(def ^:private concat*
-  (fnil (comp vec concat) []))
+            [clojure.pprint :refer [pprint]]
+            [stowaway.datalog :as dtl]))
 
 (def ^:private conj*
   (fnil conj []))
@@ -14,29 +12,9 @@
    :query-prefix []
    :remap {}})
 
-(defn- coerce
-  [x]
-  ((:coerce *opts*) x))
-
-(defn- args-key []
-  (:args-key *opts*))
-
 (defn- remap
   [k]
   (get-in (:remap *opts*) [k] k))
-
-(defn- query-key
-  [& ks]
-  (concat (:query-prefix *opts*) ks))
-
-(defmulti apply-criterion
-  (fn [_query [_k v]]
-    (when (vector? v)
-      (case (first v)
-        :=                  :direct
-        (:< :<= :> :>= :!=) :binary-pred
-        :and                :intersection
-        :or                 :union))))
 
 (defn- id?
   "Returns true if the given keyword specifies an entity id"
@@ -54,98 +32,6 @@
                   (name k))
                 suffix))))
 
-(defn- param-ref
-  "Given an attribute keyword, return a symbol that will represent
-  an input value in the query"
-  [k]
-  (if (id? k)
-    '?x
-    (attr-ref k "-in")))
-
-(defn- append-where
-  "Appends clauses to an existing where clause.
-
-  When given a predicate, appends an assignment clause and a predicate clause.
-
-  [[?x :foo/bar ?bar]
-   [(>= ?bar ?bar-in)]]
-
-  When not given a predicate, appends an assignment clause (which is implicitly
-  an equality test as well).
-
-  [[?x :foo/bar ?bar-in]]"
-  ; NB if we weren't expecting to have (pull ?x [*]) in the :find clause,
-  ; then we'd need to bind to the reference without in "-in" suffix.
-  ([w k input]
-   (append-where w k input nil))
-  ([w k input pred]
-   (let [attr (attr-ref k)
-         assignment-ref (if pred
-                          attr
-                          input)
-         assignment (when-not (id? k)
-                      ['?x
-                       (remap k)
-                       assignment-ref])
-         predicate (when pred
-                     [(list (symbol (name pred))
-                            attr
-                            input)])]
-     (concat* w
-              (filterv identity
-                       [assignment
-                        predicate])))))
-
-(defn- apply-simple-criterion
-  [query k v]
-  (let [input (param-ref k)]
-    (-> query
-        (update-in (query-key :where) append-where k input)
-        (update-in (query-key :in) conj* input)
-        (update-in (args-key) conj* (coerce v)))))
-
-(defmethod apply-criterion :default
-  [query [k v]]
-  (apply-simple-criterion query k v))
-
-(defmethod apply-criterion :direct
-  [query [k [_oper v]]]
-  (apply-simple-criterion query k v))
-
-(defmethod apply-criterion :binary-pred
-  [query [k [pred v]]]
-  (let [input (attr-ref k "-in")]
-    (-> query
-        (update-in (args-key) conj* (coerce v))
-        (update-in (query-key :in) conj* input)
-        (update-in (query-key :where) append-where k input pred))))
-
-(defmethod apply-criterion :intersection
-  [query [k [_and & vs]]]
-  ; 1. establish a reference to the model attribute
-  ; 2. apply each comparison to the reference
-  ; 3. decide if we need to wrap with (and ...)
-  (let [attr (name k)
-        input-refs (map (comp symbol
-                              #(str "?" attr "-in-" %)
-                              #(+ 1 %))
-                        (range (count vs)))
-        attr-ref (attr-ref k)]
-    (-> query
-        (update-in (args-key) concat* (map (comp coerce last) vs))
-        (update-in (query-key :in)      concat* input-refs)
-        (update-in (query-key :where)
-                   concat*
-                   (cons
-                     ['?x (remap k) attr-ref]
-                     (->> vs
-                        (interleave input-refs)
-                        (partition 2)
-                        (map (fn [[input-ref [oper]]]
-                               [(list (-> oper name symbol)
-                                       attr-ref
-                                       input-ref)]))))))))
-
 (s/def ::args-key (s/coll-of keyword? :kind vector?))
 (s/def ::query-prefix (s/coll-of keyword :kind vector?))
 (s/def ::options (s/keys :opt-un [::args-key
@@ -156,26 +42,12 @@
   `(binding [*opts* (merge *opts* ~opts)]
      ~@body))
 
-(defn- apply-join
-  [query _criterion _opts]
-  query)
-
-(defn- join-and-apply-criterion
-  [opts]
-  (fn [qry criterion]
-    (-> qry
-        (apply-join criterion opts)
-        (apply-criterion criterion))))
-
 (defn apply-criteria
   [query criteria & {:as opts}]
   {:pre [(or (nil? opts)
              (s/valid? ::options opts))]}
-
-  (with-options opts criteria
-    (reduce (join-and-apply-criterion opts)
-            query
-            criteria)))
+  (dtl/apply-criteria query criteria (assoc opts :relationships #{[:user :entity]
+                                                                  [:entity :commodity]})))
 
 (defn- ensure-attr
   [{:keys [where] :as query} k arg-ident]
