d30462377288a595a29bdc0e33d504f680a28b26
Doug Knight <dougbknight@gmail.com>
2024-06-19 13:16:08 -0500

    remove code moved to stowaway

--- src/multi_money/datalog.clj
+++ src/multi_money/datalog.clj
@@ -3,87 +3,15 @@
             [clojure.pprint :refer [pprint]]
             [stowaway.datalog :as dtl]))
 
-(def ^:private conj*
-  (fnil conj []))
-
-(def ^{:private true :dynamic true} *opts*
-  {:coerce identity
-   :args-key [:args]
-   :query-prefix []
-   :remap {}})
-
-(defn- remap
-  [k]
-  (get-in (:remap *opts*) [k] k))
-
-(defn- id?
-  "Returns true if the given keyword specifies an entity id"
-  [k]
-  (= :id (remap k)))
-
-(defn- attr-ref
-  "Given an attribute keyword, return a symbol that will represent
-  the value in the query."
-  ([k] (attr-ref k nil))
-  ([k suffix]
-   (symbol (str "?"
-                (if (id? k)
-                  "x"
-                  (name k))
-                suffix))))
-
-(s/def ::args-key (s/coll-of keyword? :kind vector?))
-(s/def ::query-prefix (s/coll-of keyword :kind vector?))
-(s/def ::options (s/keys :opt-un [::args-key
-                                  ::query-prefix]))
-
-(defmacro ^:private with-options
-  [opts & body]
-  `(binding [*opts* (merge *opts* ~opts)]
-     ~@body))
+(def ^:private default-opts
+  {:relationships #{[:user :entity]
+                    [:entity :commodity]}
+   :query-prefix [:query]})
 
 (defn apply-criteria
   [query criteria & {:as opts}]
   {:pre [(or (nil? opts)
              (s/valid? ::options opts))]}
-  (dtl/apply-criteria query criteria (assoc opts :relationships #{[:user :entity]
-                                                                  [:entity :commodity]})))
-
-(defn- ensure-attr
-  [{:keys [where] :as query} k arg-ident]
-  (if (some #(= arg-ident (last %))
-            where)
-    query
-    (update-in query [:where] conj* ['?x k arg-ident])))
-
-(defmulti apply-sort-segment
-  (fn [_query seg]
-    (when (vector? seg) :vector)))
-
-(defmethod apply-sort-segment :default
-  [query seg]
-  (apply-sort-segment query [seg :asc]))
-
-(defmethod apply-sort-segment :vector
-  [query [k dir]]
-  (let [arg-ident (attr-ref k)]
-    (-> query
-        (ensure-attr k arg-ident)
-        (update-in [:find] conj* arg-ident)
-        (update-in [:order-by] conj* [arg-ident dir]))))
-
-(defn- apply-sort
-  [query order-by]
-  (reduce apply-sort-segment
-          query
-          (if (coll? order-by)
-            order-by
-            [order-by])))
-
-(defn apply-options
-  [query {:keys [limit offset order-by]} & {:as opts}]
-  (with-options opts
-    (cond-> query
-      limit (assoc :limit limit)
-      offset (assoc :offset offset)
-      order-by (apply-sort order-by))))
+  (dtl/apply-criteria query
+                      criteria
+                      (merge default-opts opts)))
