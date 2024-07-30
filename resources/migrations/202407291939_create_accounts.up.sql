94f80aecf70406616c49593d1c3c180319fde718
Doug Knight <dougbknight@gmail.com>
2024-07-29 19:50:28 -0500

    WIP start working on SQL accounts

---
+++ resources/migrations/202407291939_create_accounts.up.sql
@@ -0,0 +1,13 @@
+create table accounts (
+  id serial,
+  entity_id int not null references entities (id) on delete cascade,
+  commodity_id int not null references commodities (id),
+  name varchar(100) NOT NULL,
+  type varchar(10) NOT NULL,
+  created_at timestamp with time zone DEFAULT now() NOT NULL,
+  updated_at timestamp with time zone DEFAULT now() NOT NULL,
+  primary key (id)
+);
+create unique index uk_accountss_name on accounts(entity_id, name);
+grant select, insert, update, delete on entities to app_user;
+grant select, insert, update, delete on entities_id_seq to app_user;
--- src/multi_money/db/sql/accounts.clj
+++ src/multi_money/db/sql/accounts.clj
@@ -1,5 +1,36 @@
 (ns multi-money.db.sql.accounts
-  (:require [multi-money.db.sql :as sql]))
+  (:require [clojure.set :refer [rename-keys]]
+            [dgknght.app-lib.core :refer [update-in-if]]
+            [multi-money.util :refer [->model-ref]]
+            [multi-money.db.sql :as sql]))
 
 (defmethod sql/attributes :account [_]
   [:id :name :entity-id :commodity-id :type])
+
+(defn- sqlize-ids
+  [m]
+  (-> m
+      (rename-keys {:account/entity :account/entity-id
+                    :account/commodity :account/commodity-id})
+      (update-in-if [:account/entity-id] sql/->id)
+      (update-in-if [:account/commodity-id] sql/->id)))
+
+(defn- generalize-ids
+  [m]
+  (-> m
+      (rename-keys {:account/entity-id :account/entity
+                    :account/commodity-id :account/commodity})
+      (update-in [:account/entity] ->model-ref)
+      (update-in [:account/commodity] ->model-ref)))
+
+(defmethod sql/before-save :account
+  [account]
+  (-> account
+      (update-in [:account/type] name)
+      sqlize-ids))
+
+(defmethod sql/after-read :account
+  [account]
+  (-> account
+      generalize-ids
+      (update-in [:account/type] keyword)))
