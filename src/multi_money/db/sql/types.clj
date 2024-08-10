855081817d20e17398e880a026a2c610612c53f7
Doug Knight <dougbknight@gmail.com>
2024-08-10 15:59:52 -0500

    leave uuid alone in sql/coerce-id

--- src/multi_money/db/sql/types.clj
+++ src/multi_money/db/sql/types.clj
@@ -3,17 +3,18 @@
 (derive java.lang.Integer ::integer)
 (derive java.lang.Long ::integer)
 (derive java.lang.String ::string)
+(derive java.util.UUID ::uuid)
 (derive clojure.lang.PersistentVector ::vector)
+(derive ::integer ::id)
+(derive ::uuid ::id)
 
 (defmulti coerce-id type)
+
+(defmethod coerce-id ::id [id] id)
 
 (defmethod coerce-id ::string
   [s]
   (Long/parseLong s))
-
-(defmethod coerce-id ::integer
-  [id]
-  id)
 
 (defmethod coerce-id ::vector
   [v]
