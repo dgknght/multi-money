53f3934cd54228a44035a0e21be78bd6a1bfe61d
Doug Knight <dougbknight@gmail.com>
2024-08-24 10:47:46 -0500

    adjust entity criteria

--- src/multi_money/db/sql/entities.clj
+++ src/multi_money/db/sql/entities.clj
@@ -1,5 +1,6 @@
 (ns multi-money.db.sql.entities
   (:require [clojure.pprint :refer [pprint]]
+            [multi-money.util :refer [apply-to-criteria]]
             [multi-money.db :as db]
             [multi-money.db.sql :as sql]))
 
@@ -19,3 +20,7 @@
 (defmethod sql/after-read :entity
   [entity]
   (->model-refs entity))
+
+(defmethod sql/prepare-criteria :entity
+  [criteria]
+  (apply-to-criteria criteria ->sql-refs))
