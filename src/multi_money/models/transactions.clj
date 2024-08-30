3690ccc7971a6e68d91ad25018c42b9d55cdb921
Doug Knight <dougbknight@gmail.com>
2024-08-30 15:13:21 -0500

    remove orchestraction ns

--- src/multi_money/models/transactions.clj
+++ src/multi_money/models/transactions.clj
@@ -5,7 +5,8 @@
             [java-time.api :as t]
             [dgknght.app-lib.validation :as v]
             [multi-money.db :as db]
-            [multi-money.orchestration :as orc]
+            [multi-money.util :as utl]
+            [multi-money.accounts :refer [polarize]]
             [multi-money.models.accounts :as acts]))
 
 (derive clojure.lang.PersistentVector ::vector)
@@ -140,11 +141,101 @@
     (db/model-type x :transaction)
     (find x date)))
 
+(defn- resolve-account
+  [account]
+  (if (:account/name account)
+    account
+    (acts/find account)))
+
+(def ^:private extract-debit
+  (juxt (constantly :debit)
+        (comp resolve-account
+              :transaction-item/debit-account)
+        :transaction-item/quantity))
+
+(def ^:private extract-credit
+  (juxt (constantly :credit)
+        (comp resolve-account
+              :transaction-item/credit-account)
+        :transaction-item/quantity))
+
+(defn- adj-act-balance
+  [[action account quantity]]
+  (update-in account [:account/balance] + (polarize quantity action account)))
+
+(defn- update-1st-trx-date
+  [model date k]
+  (update-in model [k] #(->> [% date]
+                               (filter identity)
+                               (sort t/before?)
+                               first)))
+
+(defn- update-last-trx-date
+  [model date k]
+  (update-in model
+             [k]
+             #(->> [% date]
+                   (filter identity)
+                   (sort t/after?)
+                   first)))
+
+(defn- affected-accounts
+  [date items]
+  (->> items
+       (mapcat (juxt extract-debit
+                     extract-credit))
+       (map (comp #(update-1st-trx-date % date :account/first-transaction-date)
+                  #(update-last-trx-date % date :account/last-transaction-date)
+                  adj-act-balance))))
+
+(defn- update-entity
+  [entity date]
+  (-> entity
+      (update-1st-trx-date date :entity/first-transaction-date)
+      (update-last-trx-date date :entity/last-transaction-date)))
+
+(defn- previous-item
+  [account date]
+  (->> (select {:transaction/account account
+                     :transaction/date [:<= date]})
+       (mapcat :transaction/items)
+       (sort-by (comp max
+                      (juxt :transaction-item/debit-index
+                            :transaction-item/credit-index)))))
+
+(defn- append-previous-items
+  [{:as m :keys [accounts transaction]}]
+  m)
+
+(defn- gather-accounts
+  [{:as m :keys [transaction]}]
+  (assoc m [:accounts] (->> (:transaction/items transaction)
+                            (mapcat (juxt :transaction-item/debit-account
+                                          :transaction-item/credit-account))
+                            (reduce (fn [res {:keys [id] :as a}]
+                                      (assoc res id (if (:account/name a)
+                                                      a
+                                                      (acts/find a))))
+                                    {}))))
+
+(defn propagate-transaction
+  [{:as transaction :transaction/keys [items date entity]}]
+  {:pre [(vector? (:transaction/items transaction))
+         (t/local-date? (:transaction/date transaction))]}
+  [(-> {:transaction transaction}
+      gather-accounts
+      append-previous-items
+      (utl/pp-> :with-previus-items)
+      :transaction
+      #_propagate-items
+      #_update-entity
+      #_extract-puts)])
+
 (defn put
   [{:as transaction :transaction/keys [date]}]
   (v/with-ex-validation transaction ::transaction
     (let [records-or-ids (db/put (db/storage)
-                                 (orc/propagate-transaction transaction))]
+                                 (propagate-transaction transaction))]
       ; TODO: return all of the saved models instead of the first?
       (resolve-put-result (first records-or-ids) date))))
 
